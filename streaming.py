b6a98bcb8b3c03313328321d1373d55ee7f9af42
rumi <102561487+newbrumi@users.noreply.github.com>
2022-03-28 15:47:38 -0800

    Create streaming.py

---
+++ streaming.py
@@ -0,0 +1,22 @@
+import discord
+import json
+from discord.ext import commands
+print("made by rumi")
+
+settings = json.load(open("config.json", "r"))
+token = settings["token"]
+status = settings["status"]
+link = settings["link"]
+
+
+
+bot = commands.Bot(command_prefix=".", description='fuck you')
+
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Streaming(name=status, url=link))
+    print("status on")
+
+bot.run(token, bot=False)
